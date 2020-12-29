2f30e0271b7d9a92e771c659b8902965ca82c8b6
Sheikh Aafaq Rashid <56719749+sheikhaafaq@users.noreply.github.com>
2020-12-29 21:16:39 +0530

    Update concatenate_array.py

--- concatenate_array.py
+++ concatenate_array.py
@@ -1,7 +1,6 @@
 import numpy as np 
 m,n,p = map(int,input().split())
-print(m , n , p)
 arr1 = np.array([input().split() for i in range(m)])
 arr2 = np.array([input().split() for i in range(n)])
 
-print(numpy.concatenate(arr1,arr2),axis =0)
+print(np.concatenate(arr1,arr2),axis =0)
